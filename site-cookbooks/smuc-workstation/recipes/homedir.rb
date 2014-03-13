d7497a1de1500157f95f8c40920cccdc10ff8676
Scott Muc <scottmuc@gmail.com>
2014-03-13 12:12:31 +0200

    adding homedir setup automation
    * shout out to Socrates and Tibo for the assist!

---
+++ site-cookbooks/smuc-workstation/recipes/homedir.rb
@@ -0,0 +1,45 @@
+# I am storing all my homedir in a git repository because when I attempted to
+# automate this with chef, I created a massively over-engineered beast.
+
+home_dir     = node['smuc-workstation'][:home]
+current_user = node['current_user']
+git_dir      = File.join(home_dir, '.git')
+
+execute "initialize git repository" do
+  cwd home_dir
+  user current_user
+  command 'git init'
+  not_if { File.exists?(git_dir) }
+end
+
+execute "add homedir repo as a remote" do
+  cwd home_dir
+  user current_user
+  command 'git remote add origin git@github.com:scottmuc/homedir.git'
+  not_if "cd #{home_dir} && git remote | grep origin"
+end
+
+execute "add homedir repo as a remote" do
+  cwd home_dir
+  user current_user
+  command 'git fetch origin master'
+end
+
+execute "fetch remote objects" do
+  cwd home_dir
+  user current_user
+  command 'git fetch --all'
+end
+
+execute "ensure master is at tip" do
+  cwd home_dir
+  user current_user
+  command 'git reset --hard origin/master'
+end
+
+execute "fetch our submodules" do
+  cwd home_dir
+  user current_user
+  command 'git submodule update --init'
+end
+
