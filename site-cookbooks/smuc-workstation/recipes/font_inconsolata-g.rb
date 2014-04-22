377fb28af94971c61abb4c3bd15b305d3bfbc0b3
Scott Muc <scottmuc@gmail.com>
2014-04-22 12:52:44 -0700

    auto installing inconsolata-g

---
+++ site-cookbooks/smuc-workstation/recipes/font_inconsolata-g.rb
@@ -0,0 +1,2 @@
+# installs Inconsolata-g font
+cookbook_file File.join(node['smuc-workstation'][:home], "/Library/Fonts/Inconsolata-g.otf")
--- site-cookbooks/smuc-workstation/recipes/font_inconsolata.rb
+++
@@ -1,4 +0,0 @@
-# installs Inconsolata font
-remote_file File.join(node['smuc-workstation'][:home], "/Library/Fonts/Inconsolata.otf") do
-  source "http://levien.com/type/myfonts/Inconsolata.otf"
-end
